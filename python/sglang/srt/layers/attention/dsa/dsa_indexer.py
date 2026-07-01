034e7f214e811f780be72bd5ad9ad7092f54d11d
yuchengliu1 <yucheng.liu@intel.com>
2026-07-01 04:06:11 -0700

    add  dsa_matadata to IntelAMXAttnBackend

--- python/sglang/srt/layers/attention/dsa/dsa_indexer.py
+++ python/sglang/srt/layers/attention/dsa/dsa_indexer.py
@@ -458,7 +458,7 @@ class Indexer(MultiPlatformOp):
             self.k_norm = RMSNorm(self.head_dim)
         else:
             self.k_norm = LayerNorm(
-                self.head_dim, dtype=torch.bfloat16 if _use_aiter else torch.float32
+                self.head_dim, dtype=torch.bfloat16 if (_use_aiter or (_is_cpu and _cpu_amx)) else torch.float32
             )
         self.rotary_emb = get_rope_wrapper(
             rope_head_dim,
@@ -670,6 +670,7 @@ class Indexer(MultiPlatformOp):
     ):
         # Non-fusion path only; self.wk does not exist when fusion is on.
         key, _ = self.wk(x)
+        key = key.to(torch.bfloat16)
         key = self.k_norm(key)
         k_rope, _ = torch.split(
             key, [self.rope_head_dim, self.head_dim - self.rope_head_dim], dim=-1
@@ -2428,7 +2429,6 @@ class Indexer(MultiPlatformOp):
             return None
 
         # Determine if should skip topk based on sequence length
-        # We can only skip the logits computation if cuda graph is not involved
         skip_logits_computation = False
         if forward_batch.forward_mode.is_extend_without_speculative():
             if forward_batch.seq_lens_cpu is not None:
